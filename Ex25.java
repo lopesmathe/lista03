b0ce1e084a0df1578ceab7fa051786a85e5d0ef2
lopesmathe <matheuslopesbauer@gmail.com>
2024-03-25 05:30:14 -0300

    ADD ex25

--- Ex25.java
+++ Ex25.java
@@ -1,4 +1,4 @@
-public class Ex24 {
+public class Ex25 {
     public static void run() {
         Wutil util = new Wutil();
         NotasAlunos notas = new NotasAlunos();
@@ -12,6 +12,20 @@ public class Ex24 {
         util.PegarNumeroSemNadaF();
         notas.nota3 = util.VoltarNumero1F();
         float media = (notas.nota1 + notas.nota2 + notas.nota3) / 3;
-        System.out.println(media);
+        if(media > 8){
+            System.out.println("A");
+        }
+        else if(media > 7 || media <8){
+            System.out.println("B");
+        }
+        else if(media > 6 || media <7){
+            System.out.println("C");
+        }
+        else if(media > 5 || media <6){
+            System.out.println("D");
+        }
+        else if(media > 0 || media <5){
+            System.out.println("R");
+        }
     }
 }
